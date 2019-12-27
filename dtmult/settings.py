8749b8639ee2b9946961f1ee2484fdd645095641
João garcia <joaogarciadelimaneto@gmail.com>
2019-12-27 17:09:04 -0300

    Configured django-debug-toolbar
    Closes #20

--- dtmult/settings.py
+++ dtmult/settings.py
@@ -77,6 +77,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dtmult.wsgi.application'
 
+# configuração django debug toolbar
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases

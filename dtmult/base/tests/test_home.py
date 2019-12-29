f3c57d41a64592e1d7ec37f8b9f3f8de38fa58c9
João garcia <joaogarciadelimaneto@gmail.com>
2019-12-29 11:57:31 -0300

    Configured templates inheritance
    Closes #28

--- dtmult/base/tests/test_home.py
+++ dtmult/base/tests/test_home.py
@@ -15,7 +15,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Dutty</title>')
+    assert_contains(resp, '<title>Dutty - Home</title>')
 
 
 def test_home_link(resp):
